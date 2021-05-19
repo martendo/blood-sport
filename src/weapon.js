ad95838fdc91a007b5bc182eb6ba721a084e34eb
martendo7 <51766598+martendo7@users.noreply.github.com>
2021-05-19 09:29:53 -0400

    Make Weapon swing in a circle

--- src/weapon.js
+++ src/weapon.js
@@ -7,72 +7,51 @@ class Weapon extends Sprite {
     this.parent = parent;
     
     this.pos = new Vector2();
-    this.animations = {
-      up: new Animation(this, this.game, {
-        images: [0, 1, 2, 3, 4, 5, 6],
-        duration: [15, 15, 15, 15, 15, 15, 15],
-      }),
-      down: new Animation(this, this.game, {
-        images: [7, 8, 9, 10, 11, 12, 13],
-        duration: [15, 15, 15, 15, 15, 15, 15],
-      }),
-      left: new Animation(this, this.game, {
-        images: [14, 15, 16, 17, 18, 19, 20],
-        duration: [15, 15, 15, 15, 15, 15, 15],
-      }),
-      right: new Animation(this, this.game, {
-        images: [21, 22, 23, 24, 25, 26, 27],
-        duration: [15, 15, 15, 15, 15, 15, 15],
-      }),
-    };
-    this.hitboxes = {
-      up: new Rect(13, 2, 6, 30),
-      down: new Rect(13, 0, 6, 30),
-      left: new Rect(2, 13, 30, 6),
-      right: new Rect(0, 13, 30, 6),
-    };
-    this.image = this.animations.right.currentImage;
+    this.animation = new Animation(this, this.game, {
+      images: [0, 1, 2, 3, 4, 5, 6, 7],
+      duration: [30, 30, 30, 30, 30, 30, 30],
+    });
+    this.image = this.animation.currentImage;
     this.rect = new Rect(
       0,
       0,
       this.image.width,
       this.image.height,
     );
-    this.hitbox = this.hitboxes.right;
-    this.direction = parent.direction;
-    this.animations.right.start();
+    this.hitbox = new Rect(2, 2, 60, 60);
+    if (this.parent.vel.y < 0) {
+      if (this.parent.vel.x < 0) {
+        this.animation.frame = 7;
+      } else if (this.parent.vel.x > 0) {
+        this.animation.frame = 1;
+      } else {
+        this.animation.frame = 0;
+      }
+    } else if (this.parent.vel.y > 0) {
+      if (this.parent.vel.x < 0) {
+        this.animation.frame = 5;
+      } else if (this.parent.vel.x > 0) {
+        this.animation.frame = 3;
+      } else {
+        this.animation.frame = 4;
+      }
+    } else {
+      if (this.parent.vel.x < 0) {
+        this.animation.frame = 6;
+      } else if (this.parent.vel.x > 0) {
+        this.animation.frame = 2;
+      } else {
+        this.animation.frame = 0;
+      }
+    }
+    this.animation.start();
     
     this.game.actors.add(this);
   }
   
   update() {
-    this.direction = this.parent.direction;
-    
-    let animation;
-    if (this.direction === this.game.DIR_UP) {
-      this.rect.centrex = this.parent.rect.centrex;
-      this.rect.bottom = this.parent.rect.centrey;
-      animation = this.animations.up;
-      this.hitbox = this.hitboxes.up;
-    } else if (this.direction === this.game.DIR_DOWN) {
-      this.rect.centrex = this.parent.rect.centrex;
-      this.rect.top = this.parent.rect.centrey;
-      animation = this.animations.down;
-      this.hitbox = this.hitboxes.down;
-    } else if (this.direction === this.game.DIR_LEFT) {
-      this.rect.centrey = this.parent.rect.centrey;
-      this.rect.right = this.parent.rect.centrex;
-      animation = this.animations.left;
-      this.hitbox = this.hitboxes.left;
-    } else if (this.direction === this.game.DIR_RIGHT) {
-      this.rect.centrey = this.parent.rect.centrey;
-      this.rect.left = this.parent.rect.centrex;
-      animation = this.animations.right;
-      this.hitbox = this.hitboxes.right;
-    }
-    this.switchAnimation(animation);
-    this.image = animation.currentImage;
-    
+    this.rect.centrex = this.parent.rect.centrex;
+    this.rect.centrey = this.parent.rect.centrey;
     this.pos.x = this.rect.left;
     this.pos.y = this.rect.bottom;
   }
